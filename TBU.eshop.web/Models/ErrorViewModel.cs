278c764f99af465bfce9a39eba9e3a2d526ce2d3
FunTom <vogeltanz@utb.cz>
2022-02-10 10:52:30 +0100

    Add project files.

---
+++ TBU.eshop.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TBU.eshop.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
