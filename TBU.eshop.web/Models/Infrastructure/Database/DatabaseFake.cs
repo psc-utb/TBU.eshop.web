58b4bcb26539a7efa2b3ab811993528bfea4fc24
FunTom <vogeltanz@utb.cz>
2022-02-16 23:08:52 +0100

    DatabaseFake & DatabaseInit Added DatabaseFake class which simulates the database. Added DatabaseInit class which initializes the database with CarouselItem data. The classes have the code commented (CarouselItem class is necessary) (Fourth slide change in alt attribute in Index.cshtml)

---
+++ TBU.eshop.web/Models/Infrastructure/Database/DatabaseFake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TBU.eshop.web.Models.Infrastructure.Database
+{
+    public static class DatabaseFake
+    {
+        /*public static List<CarouselItem> CarouselItems { get; set; }
+        static DatabaseFake()
+        {
+            DatabaseInit dbInit = new DatabaseInit();
+            CarouselItems = dbInit.GenerateCarouselItems();
+        }*/
+    }
+}
---
+++ TBU.eshop.web/Models/Infrastructure/Database/DatabaseInit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TBU.eshop.web.Models.Infrastructure.Database
+{
+    public class DatabaseInit
+    {
+        /*public List<CarouselItem> GenerateCarouselItems()
+        {
+            List<CarouselItem> carouselItems = new List<CarouselItem>();
+            CarouselItem ci1 = new CarouselItem()
+            {
+                ID = 0,
+                ImageSource = "/img/Information-Technology.jpg",
+                ImageAlt = "First slide"
+            };
+            CarouselItem ci2 = new CarouselItem()
+            {
+                ID = 1,
+                ImageSource = "/img/Information-Technology-1.jpg",
+                ImageAlt = "Second slide"
+            };
+            CarouselItem ci3 = new CarouselItem()
+            {
+                ID = 2,
+                ImageSource = "/img/IT.jpeg",
+                ImageAlt = "Third slide"
+            };
+            CarouselItem ci4 = new CarouselItem()
+            {
+                ID = 3,
+                ImageSource = "/img/bits-e1611847851285-1440x810.jpeg",
+                ImageAlt = "Fourth slide"
+            };
+
+            carouselItems.Add(ci1);
+            carouselItems.Add(ci2);
+            carouselItems.Add(ci3);
+            carouselItems.Add(ci4);
+
+            return carouselItems;
+        }*/
+    }
+}
