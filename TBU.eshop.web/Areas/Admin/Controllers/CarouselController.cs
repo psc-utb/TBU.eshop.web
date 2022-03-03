4bbc9e883519de3ec6333eceb6033c12d6960d8d
psc-utb <psc.utb@gmail.com>
2022-03-03 11:36:25 +0100

    Carousel Administration - Added: Admin Area - Added: _ViewStart and _ViewImports in Area Admin - Added: CarouselController - Added: Select View for Carousel Controller - Added: Create View for Carousel Controller - Added Areas routing to Startup

---
+++ TBU.eshop.web/Areas/Admin/Controllers/CarouselController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBU.eshop.web.Models.Entities;
+using TBU.eshop.web.Models.Infrastructure.Database;
+
+namespace TBU.eshop.web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarouselController : Controller
+    {
+        public IActionResult Select()
+        {
+            IList<CarouselItem> carouselItems = DatabaseFake.CarouselItems;
+
+            return View(carouselItems);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CarouselItem carouselItem)
+        {
+            DatabaseFake.CarouselItems.Add(carouselItem);
+
+            return RedirectToAction(nameof(Select));
+        }
+    }
+}
--- TBU.eshop.web/Startup.cs
+++ TBU.eshop.web/Startup.cs
@@ -49,6 +49,11 @@ namespace TBU.eshop.web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
