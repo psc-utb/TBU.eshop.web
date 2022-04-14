9415539b5f5270bef14a03bcc3842167697abc44
Tomáš Vogeltanz <vogeltanz@utb.cz>
2022-04-14 09:46:46 +0200

    Register and Login forms (views) - login.css file for login and register forms added in webroot/css - RenderSection for Styles added to _Layout.cshtml - _ViewImports.cshtml added to Security area - Login and Register views added to Security area - LoginViewModel and RegisterViewModel added in Model

--- TBU.eshop.web/Models/ViewModel/LoginViewModel.cs
+++ TBU.eshop.web/Models/ViewModel/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace TBU.eshop.web.Models.ViewModel
 {
     public class LoginViewModel
     {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool LoginFailed { get; set; }
     }
 }
--- TBU.eshop.web/Models/ViewModel/RegisterViewModel.cs
+++ TBU.eshop.web/Models/ViewModel/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,22 @@ namespace TBU.eshop.web.Models.ViewModel
 {
     public class RegisterViewModel
     {
+        [Required]
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Required]
+        //[UniqueCharacters(2)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{2,}$", ErrorMessage = RegisterViewModel.ErrorMessagePassword)]
+        public string Password { get; set; }
+        private const string ErrorMessagePassword = "Passwords must be at least 2 characters.<BR>Passwords must have at least one non alphanumeric character.<BR>Passwords must have at least one digit('0'-'9').<BR>Passwords must have at least one uppercase('A'-'Z').";
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords don't match!")]
+        public string RepeatedPassword { get; set; }
     }
 }
